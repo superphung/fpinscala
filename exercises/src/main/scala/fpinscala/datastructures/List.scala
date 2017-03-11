10ff8af262ce3aace51c8433f985ed97b844c473
superphung <superphung44@gmail.com>
2017-03-11 22:18:52 +0100

    add drop

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -62,7 +62,12 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(_, t) => Cons(h, t)
     }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = 
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(h, t) => drop(t, n - 1)
+    }
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
 
